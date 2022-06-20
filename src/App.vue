c4c2afb86084a3284cbd025b037bc867d4e059a1
denghao1999 <1429751549@qq.com>
2022-06-20 21:08:22 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
     <h1>黑马头条</h1>
     <!-- 路由出口 -->
     <router-view/>
+    <div>
+     <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
